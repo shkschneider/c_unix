0f493141c9a4ba28cf6846622439734dd2c99906
shkschneider <shk.schneider@gmail.com>
2012-01-08 14:02:16 +0100

    cfree: handles malloc return value

--- malloc/cfree/cmalloc.c
+++ malloc/cfree/cmalloc.c
@@ -14,13 +14,14 @@ void * cmalloc(size_t size, const char * func, const char * file, const int line
   void * p;
   char * s;
 
-  p = malloc(size);
-  s = malloc(sizeof(char) * INFO_MAX);
-  snprintf(s, INFO_MAX, "%s:%s() l.%d", file, func, line);
-  if (!gl_cfree)
-    gl_cfree = cfree_list_create(p, size, s);
-  else
-    gl_cfree = cfree_list_add_first(gl_cfree, p, size, s);
-  gl_cfree_c++;
+  if ((p = malloc(size))) {
+    s = malloc(sizeof(char) * INFO_MAX);
+    snprintf(s, INFO_MAX, "%s:%s() l.%d", file, func, line);
+    if (!gl_cfree)
+      gl_cfree = cfree_list_create(p, size, s);
+    else
+      gl_cfree = cfree_list_add_first(gl_cfree, p, size, s);
+    gl_cfree_c++;
+  }
   return (p);
 }
