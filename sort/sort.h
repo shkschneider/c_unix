c1e5960e40360484f8655ee4a8fa08bcf91595fa
shkschneider <shk.schneider@gmail.com>
2016-05-11 13:27:07 +0200

    sort: sort.h

---
+++ sort/sort.h
@@ -0,0 +1,13 @@
+#ifndef _SORT_H_
+# define _SORT_H_
+
+void bubble_sort(int *tabl, int size);
+void fusion_sort(int *tabl, int size);
+void heap_sort(int *tabl, int size);
+void insertion_sort(int *tabl, int size);
+void quick_sort(int *tabl, int size);
+void radix_sort(int *tabl, int size);
+void selection_sort(int *tabl, int size);
+void shell_sort(int *tabl, int size);
+
+#endif /* !_SORT_H_ */
